01e478fef1e4b7f26056bb29d732e4078acf6373
bouimust <moussovic@gmail.com>
2023-01-05 08:39:04 +0100

    home page

--- index.php
+++ index.php
@@ -12,32 +12,104 @@
         unset($_SESSION['message']);
     };
     ?>
-    <div class="bg-dark mb-2" style="border: 2px solid #666;">
-        <h4 class="text-center pt-2">&#x1F4E3; MES REALISATIONS &#x1F4E3;</h4>
+
+    <!-- BLOC PROJECTS -->
+    <div class="my-2">
+        <div class="border border-dark">
+            <h4 class="text-center pt-2">&#x1F4E3; MES REALISATIONS &#x1F4E3;</h4>
+        </div>
+        <div class="border border-dark p-2">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+        </div>
     </div>
-    <div class="card bg-dark" style="border: 2px solid #666;">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+
+    <!-- BLOC SKILLS -->
+    <div class="my-2">
+        <div class="border border-dark">
+            <h4 class="text-center pt-2">&#x1F3AF; MES COMPETENCES &#x1F3AF;</h4>
+        </div>
+        <div class="border border-dark p-2">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+        </div>
     </div>
-    <div class="bg-dark mb-2" style="border: 2px solid #666;">
-        <h4 class="text-center pt-2">&#x1F3AF; MES COMPETENCES &#x1F3AF;</h4>
+
+    <!-- BLOC PARCOURS -->
+    <div class="my-2">
+        <div class="border border-dark">
+            <h4 class="text-center pt-2">&#x1F3F2; MON PARCOURS &#x1F3F2;</h4>
+        </div>
+        <div class="border border-dark p-2">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+        </div>
     </div>
-    <div class="card bg-dark" style="border: 2px solid #666;">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-    </div>
-    <div class="bg-dark mb-2" style="border: 2px solid #666;">
-        <h4 class="text-center pt-2">&#x1F3F2; MON PARCOURS &#x1F3F2;</h4>
-    </div>
-    <div class="card bg-dark" style="border: 2px solid #666;">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio fugit, </p>
+
+    <!-- BLOC MESSAGES -->
+    <div></div>
+
+    <!-- BLOC NEW MESSAGE -->
+
+    <div class="my-2">
+        <div class="border border-dark">
+            <h4 class="text-center pt-1">Ajouter un message</h4>
+        </div>
+        <div class="border border-dark">
+            <div class="col-6 mx-auto py-3">
+                <form action="./core/messageController.php" method="post">
+
+                    <div class="row justify-content-center mx-auto">
+
+                        <input type="hidden" name="action" value="create">
+                        <input type="hidden" name="page" value=<?= $_SERVER['SCRIPT_NAME'] ?>>
+
+                        <div class="col-6">
+                            <div class="my-1">
+                                <label for="last_name">Nom * :</label>
+                                <input class="form-control pointer border border-dark" type="text" name="last_name" id="last_name">
+                            </div>
+                            <div class="my-1">
+                                <label for="first_name">Prénom * :</label>
+                                <input class="form-control pointer border border-dark" type="text" name="first_name" id="first_name">
+                            </div>
+                            <div class="my-1">
+                                <label for="email">Adresse email * :</label>
+                                <input class="form-control pointer border border-dark" type="email" name="email" id="email">
+                            </div>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="my-1">
+                                <label for="company">Société :</label>
+                                <input class="form-control pointer border border-dark" type="text" name="company" id="company">
+                            </div>
+                            <div class="my-1">
+                                <label for="phone">Téléphone :</label>
+                                <input class="form-control pointer border border-dark" type="tel" name="phone" id="phone">
+                            </div>
+                        </div>
+
+
+                        <div class="my-1">
+                            <label for="message">Message * :</label>
+                            <textarea class="form-control pointer border border-dark" name="message" id="message" rows="3"></textarea>
+                        </div>
+
+                        <div class="my-3">
+                            <button type="submit" name="submit" class="btn btn-success border border-dark w-100">ENREGISTRER</button>
+                        </div>
+                        <p>* : champ obligatoire</p>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </main>
 
