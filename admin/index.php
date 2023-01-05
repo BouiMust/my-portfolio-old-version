17c0607869f1a582051066dc84dff35bb15ec395
bouimust <moussovic@gmail.com>
2023-01-05 08:43:16 +0100

    login page to access back-office

--- admin/index.php
+++ admin/index.php
@@ -4,13 +4,22 @@
 <?php include '../assets/inc/front/head.php' ?>
 <title>Connexion</title>
 
+<!-- Vérifie si l'admin est dejà connecté -->
+<?php
+if (isset($_SESSION['isLog'], $_SESSION['role'])) {
+    if ($_SESSION['isLog'] && $_SESSION['role'] === '1') {
+        exit(header('Location: http://localhost/portfolio/admin/dashboardAdmin.php'));
+    }
+}
+?>
+
 <?php include '../assets/inc/front/header.php' ?>
 
 <main>
-    <div class="bg-dark mb-2" style="border: 2px solid #666;">
+    <div class="mb-2" style="border: 2px solid #666;">
         <h4 class="text-center pt-2">CONNEXION</h4>
     </div>
-    <div class="card bg-dark" style="border: 2px solid #666;">
+    <div style="border: 2px solid #666;">
         <div class="col-4 mx-auto py-4">
             <?php
             if (isset($_SESSION['message'])) {
@@ -21,10 +30,10 @@
             <form class="form-group" action="../core/userController.php" method="post">
                 <input type="hidden" name="action" value="login">
                 <label for="email">Adresse email :</label>
-                <input type="email" class="form-control my-2" name="email" id="email">
+                <input type="email" class="form-control my-2 border border-dark" name="email" id="email">
                 <label for="password">Mot de passe :</label>
-                <input type="password" class="form-control my-2" name="password" id="password">
-                <button class="btn btn-success w-100 my-4" type="submit" name="submit">CONNEXION</button>
+                <input type="password" class="form-control my-2 border border-dark" name="password" id="password">
+                <button class="btn btn-success border border-dark w-100 my-4" type="submit" name="submit">CONNEXION</button>
             </form>
         </div>
     </div>
