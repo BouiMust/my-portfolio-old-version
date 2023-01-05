263136290ea000995588fda7f19983b52b249204
bouimust <moussovic@gmail.com>
2023-01-05 08:39:17 +0100

    contact page

--- contact.php
+++ contact.php
@@ -10,18 +10,17 @@ if (isset($_POST['submit'])) {
     } else {
         $_SESSION["message"] = '<p class="alert alert-success fs-5 text-center p-1 w-50 mx-auto">Votre formulaire a été envoyé.</p>';
     }
-    header('Location: ./contact.php');
-    exit();
+    exit(header('Location: ./contact.php'));
 }
 ?>
 
 <?php include './assets/inc/front/header.php' ?>
 
 <main>
-    <div class="bg-dark mb-2" style="border: 2px solid #666;">
+    <div class="mb-2" style="border: 2px solid #666;">
         <h4 class="text-center pt-2">&#x260E; CONTACT &#x260E;</h4>
     </div>
-    <div class="card bg-dark mx-auto" style="border: 2px solid #666;">
+    <div class="mx-auto py-4" style="border: 2px solid #666;">
         <div class="card-body">
             <p class="card-text text-center">Vous avez des questions ou vous souhaitez simplement me contacter ? Remplissez ce formulaire.</p>
             <?php
@@ -32,12 +31,12 @@ if (isset($_POST['submit'])) {
             ?>
             <form action="" method="post" style="width:40%" class="form-group mx-auto">
                 <label for="name">Nom :</label>
-                <input class="form-control my-2" type="text" name="name" id="name">
+                <input class="form-control my-2 pointer border border-dark" type="text" name="name" id="name">
                 <label for="email">Adresse email :</label>
-                <input class="form-control my-2" type="email" name="email" id="email">
+                <input class="form-control my-2 pointer border border-dark" type="email" name="email" id="email">
                 <label for="message">Message :</label>
-                <textarea class="form-control my-2" name="message" id="message" rows="4" placeholder="Votre message"></textarea>
-                <button type="submit" name="submit" class="btn btn-success w-100 my-2">ENVOYER</button>
+                <textarea class="form-control my-2 pointer border border-dark" name="message" id="message" rows="4" placeholder="Votre message"></textarea>
+                <button type="submit" name="submit" class="btn btn-success border border-dark w-100 my-2">ENVOYER</button>
             </form>
         </div>
     </div>
