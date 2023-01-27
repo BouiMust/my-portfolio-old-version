2934f3b29600c2e69f74b0e4abe51a6514e33554
bouimust <moussovic@gmail.com>
2023-01-27 10:35:42 +0100

    update home link

--- assets/inc/front/header.php
+++ assets/inc/front/header.php
@@ -4,15 +4,12 @@
         <header>
             <nav class="navbar navbar-expand-lg my-2" style="border: 2px solid #666;">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="http://localhost/portfolio">PORTFOLIO</a>
+                    <a class="navbar-brand" href="http://localhost/portfolio">ACCUEIL</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="http://localhost/portfolio">ACCUEIL</a>
-                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="http://localhost/portfolio/contact.php">CONTACT</a>
                             </li>
