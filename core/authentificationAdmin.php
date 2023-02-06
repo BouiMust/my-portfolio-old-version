3ab71541f4772fdc3b0eb3980ab17bd59367f8a7
bouimust <moussovic@gmail.com>
2023-02-06 16:28:50 +0100

    authentification function

--- core/authentificationAdmin.php
+++ core/authentificationAdmin.php
@@ -1,7 +1,7 @@
 <!-- Vérifie si l'utilisateur est admin, en analysant les données en session -->
 <?php
 if (!($_SESSION['isLog'] && $_SESSION['role'] === '1')) {
-    $_SESSION['message'] = '<p class="alert alert-danger fs-5 text-center p-1">Accès refusé.</p>';
+    $_SESSION['message'] = '<p class="alert alert-danger fs-5 text-center p-1">Accès refusé : vous n\'avez pas les droits d\'administrateur.</p>';
     exit(header('Location: http://localhost/portfolio/'));
 }
 ?>
