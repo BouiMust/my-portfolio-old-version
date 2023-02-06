b826f837d56aba83eb0fa8443953db9dd3b7b576
bouimust <moussovic@gmail.com>
2023-02-06 16:26:10 +0100

    components

--- assets/inc/back/header.php
+++ assets/inc/back/header.php
@@ -4,24 +4,21 @@
         <header>
             <nav class="navbar navbar-expand-lg my-2" style="border: 2px solid #666;">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="http://localhost/portfolio/admin/dashboardAdmin.php">TABLEAU DE BORD</a>
+                    <a class="navbar-brand" href="http://localhost/portfolio/admin/dashboardAdmin.php">&#128451; TABLEAU DE BORD</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="http://localhost/portfolio">ACCUEIL</a>
-                            </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     GESTION
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="http://localhost/portfolio/admin/user">Utilisateurs</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/portfolio/admin/skill">Compétences</a></li>
-                                    <li><a class="dropdown-item" href="http://localhost/portfolio/admin/message">Messages</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/portfolio/admin/project">Réalisations</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/portfolio/admin/message">Messages</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/portfolio/admin/user">Utilisateurs</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
@@ -29,11 +26,14 @@
                                     NOUVEAU
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="http://localhost/portfolio/admin/user/createUser.php">Utilisateur</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/portfolio/admin/skill/createSkill.php">Compétence</a></li>
-                                    <li><a class="dropdown-item" href="http://localhost/portfolio/admin/message/createMessage.php">Message</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/portfolio/admin/project/createProject.php">Réalisation</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/portfolio/admin/message/createMessage.php">Message</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/portfolio/admin/user/createUser.php">Utilisateur</a></li>
                                 </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="http://localhost/portfolio">&#127962; RETOUR ACCUEIL</a>
                             </li>
                         </ul>
                         <form action="http://localhost/portfolio/core/userController.php" method="post">
--- assets/inc/front/footer.php
+++ assets/inc/front/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="text-center py-2">Portfolio Copyright © 2022. All Rights Reserved.</div>
+    <div class="text-center text-light py-2">Portfolio Copyright © 2022. All Rights Reserved.</div>
 </footer>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
